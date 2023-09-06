ea08f8db7e495252457c6cddcd63ef4ed4eff46a
MahadBulle <mahadhussein012@gmail.com>
2023-09-06 01:34:33 -0700

    Crud Operation nest js

---
+++ src/book/schemas/book.schema.ts
@@ -0,0 +1,32 @@
+import { Schema,Prop,SchemaFactory } from "@nestjs/mongoose";
+
+export enum Category {
+    ADVENTURE = 'Adventure',
+    CALSICCS = 'Classics',
+    CRIME = 'Crime ',
+    FANTASY = 'Fantasy',
+}
+
+@Schema({
+    timestamps : true,
+})
+
+export class Book {
+    @Prop()
+    title: string;
+
+    @Prop()
+    description: string;
+
+    @Prop()
+    author: string;
+
+    @Prop()
+    price: number;
+
+    @Prop()
+    category: Category
+}
+
+// export const BookSchema = crea
+export const BookSchema = SchemaFactory.createForClass(Book)
